897673b86082ccde864d2898dfe6a5334d74f0f1
Gbenga <auctionnetworkers@gmail.com>
2023-02-16 12:04:30 -0800

    Puts character

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main -Entry point 
- * Return always 0 (succes)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	/* Return always 0 for success */
 	return (0);
 }
