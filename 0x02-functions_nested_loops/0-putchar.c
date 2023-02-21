58b9b5ff336e0955cc622aae44b4da8f6a0d0573
Gbenga <auctionnetworkers@gmail.com>
2023-02-21 04:47:13 -0800

    PutChar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * success - return (0) if program runs well
+ *
+ */
+int main(void)
+{
+	char school[] = "_putchar";
+
+	printf("%s\n", school);
+	return (0);
+}
