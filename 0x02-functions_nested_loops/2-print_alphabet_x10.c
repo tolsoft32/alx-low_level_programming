e0240e8df2a79a8c2040ee7172ea00b1facb9314
Gbenga <auctionnetworkers@gmail.com>
2023-02-21 15:59:47 -0800

    nested loop

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,10 +5,10 @@
  */
 void print_alphabet_x10(void)
 {
+	int i;
 	char c;
-	int i = 0;
 
-	for (i = 'a'; i <= 'z'; i++)
+	for (i = 1; i <= 10; i++)
 	{
 		for (c = 'a'; c <= 'z'; c++)
 			_putchar(c);
