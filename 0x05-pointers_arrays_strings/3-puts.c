35727486ade15f85b6d132abf861d5f12a385b5d
Gbenga <auctionnetworkers@gmail.com>
2023-02-27 07:49:56 -0800

    Printing strings

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - Print a string
+ * @str: The string toprint
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+
+	_putchar('\n');
+}
