9529946f054b979714c69817cca26b174d1b4197
Gbenga <auctionnetworkers@gmail.com>
2023-03-01 15:51:17 -0800

    Infinite add

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,6 +10,8 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
+int _putchar(char c);
 void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 #endif /* Closing main.h */
