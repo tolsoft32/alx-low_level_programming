5fbebf99ea95c535aa7aa336b7973924e49f86c4
Gbenga <auctionnetworkers@gmail.com>
2023-03-14 04:53:00 -0700

    malloc create array or return NULL

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,17 +13,16 @@ char *create_array(unsigned int size, char c)
 	char ar;
 	unsigned int i;
 
-	i = 0;
-	if (size != 0)
-		ar = malloc(size * sizeof(c));
-	else
+	if (size == 0)
 		return (NULL);
+
+	ar = malloc(size * sizeof(char));
+
 	if (ar == NULL)
 		return (NULL);
-	while (i < size)
-	{
+
+	for (i = 0; i < size; i++)
 		ar[i] = c;
-		i++;
-	}
+
 	return (ar);
 }
