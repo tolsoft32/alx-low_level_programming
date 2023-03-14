91a7cc654a71f7505ea2c6d6cd734250ec8b6c12
Gbenga <auctionnetworkers@gmail.com>
2023-03-14 05:16:46 -0700

    malloc null array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	ar = malloc(size * sizeof(char));
+	ar = (char *)malloc(size * sizeof(char));
 
 	if (ar == NULL)
 		return (NULL);
