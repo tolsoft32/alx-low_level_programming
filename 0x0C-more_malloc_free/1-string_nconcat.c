9b9645f34ca74ddd0d7f06971fade03bafb81881
Gbenga <auctionnetworkers@gmail.com>
2023-03-16 07:11:46 -0700

    Cocatenation of two strings

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include "stdlib.h"
+
+/**
+ * string_nconcat - declaring cocatenation of two strings
+ * @s1: string one
+ * @s2: string two for cocatenation
+ * @n: numbers or length of times strings access memory
+ * Return: if the function fail, otherwise a pointer to return
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *concat;
+
+	usigned int length = n, index;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	for (index = 0; s1[index]; index++)
+		length++;
+
+	concat = malloc(sizeof(char) * (length + 1));
+
+	if (concat == NULL)
+		return (NULL);
+
+	length = 0;
+
+	for (index = 0; s1[index]; index++)
+		concat[length++] = s1[index];
+
+	for (index = 0; s2[index] && index < n; index++)
+		concat[length++] = s2[index];
+
+	concat[length] = '\0';
+
+	return (concat);
+}
