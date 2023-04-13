543eef81def82412bbaad46c5033efd7525480a6
Gbenga <auctionnetworkers@gmail.com>
2023-04-13 13:24:06 -0700

    binary to unit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,22 +9,27 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0, mult = 1;
-	int len;
+	unsigned int num = 0;
+	int i;
 
-	if (b == '\0')
+	if (b == NULL)
 		return (0);
 
-	for (len = 0; b[len];)
-		len++;
-
-	for (len -= 1; len >= 0; len--)
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (b[len] != '0' && b[len] != '1')
+		if (b[i] == '1')
+		{
+			num = num << 1;
+			num = num | 1;
+		}
+		else if (b[i] == '0')
+		{
+			num = num << 1;
+		}
+		else
+		{
 			return (0);
-
-		num += (b[len] - '0') * mult;
-		mult *= 2;
+		}
 	}
 
 	return (num);
