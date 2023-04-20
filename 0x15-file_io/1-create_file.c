61b92b227157f2db1983cb41e676327359150d9c
Gbenga <auctionnetworkers@gmail.com>
2023-04-19 23:45:13 -0700

    Create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONGLY | O_TRUNC, S_TRSUR | S_TWUSR);
+	fd = open(filename, O_CREAT | O_WRONGLY | O_TRUNC, S_IRSUR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 
