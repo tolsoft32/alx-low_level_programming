6c5e98016ec1290306706c393d1efdedf6f331e8
Gbenga <auctionnetworkers@gmail.com>
2023-04-19 23:40:35 -0700

    Create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 			length++;
 
 		f_written = write(fd, text_content, length);
-		if (f_written == -1 || f_written == length)
+		if (f_written == -1 || f_written != length)
 		{
 			close(fd);
 			return (-1);
