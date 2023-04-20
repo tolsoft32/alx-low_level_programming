205abcef3b206aa1371896267ba312c49df0f807
Gbenga <auctionnetworkers@gmail.com>
2023-04-19 23:14:34 -0700

    Read file text

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,10 +16,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t f_read, f_written;
 	char *buffer;
 
-	if (filename)
+	if (!filename)
 		return (0);
 
-	fd = ope(filename, RD_ONLY);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 
@@ -36,7 +36,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 
 	f_written = write(STDOUT_FILENO, buffer, f_read);
-	if (f_written == -1 || f_written == f_read)
+	if (f_written == -1 || f_written != f_read)
 	{
 		free(buffer);
 		close(fd);
