fc0c99587c79fefa330895925390d2d9e559aef6
Gbenga <tolsoft32@gmail.com>
2023-07-08 21:23:12 -0700

    get bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,13 +10,13 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int a;
+	unsigned int mask = 1UL << index;
 
-	if (index > 63)
+	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 
-	x = 1 << index;
-	*n = (*n | a);
-
-	return (-1);
+	if ((n & mask) != 0)
+		return (1);
+	else
+		return (0);
 }
