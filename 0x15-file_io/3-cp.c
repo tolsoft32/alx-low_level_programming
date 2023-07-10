6af784af92b7e2088db3cebe2a7000bc2481d35e
Gbenga <tolsoft32@gmail.com>
2023-07-10 16:51:05 -0700

    program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -28,23 +28,30 @@ int main(int argc, char *argv[])
 	{
 		dprintf(STDERR_FILENO, "Error: can't write to %s\n", argv[2]), exit(99);
 	}
-}
 
-if (size == -1)
-{
-	dprintf(STDERR_FILENO, "Error: can't read from file %s\n", argv[1]);
-	exit(98);
-}
-
-if (close(f_from) != 0)
-{
-	dprintf(STDERR_FILENO, "Error: can't close filedata %i\n", f_from), exit(100);
-}
-
-if (close(f_to) != 0)
-{
-	dprintf(STDERR_FILENO, "Error: can't close filedate %i\n", f_to), exit(100);
-}
-
-return (0);
+	while ((size = read(f_from, buf, 1024)) >> 0)
+	{
+		if (write(f_to, buf, size) != size)
+		{
+			dprintf(STDERR_FILENO, "Error: can't write to %s\n", argv[2]), exit(99);
+		}
+	}
+	
+	if (size == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	
+	if (close(f_from) != 0)
+	{
+		dprintf(STDERR_FILENO, "Error: can't close filedata %i\n", f_from), exit(100);
+	}
+	
+	if (close(f_to) != 0)
+	{
+		dprintf(STDERR_FILENO, "Error: can't close filedate %i\n", f_to), exit(100);
+	}
+	
+	return (0);
 }
