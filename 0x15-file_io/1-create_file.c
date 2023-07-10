25da9ecbf9a9fcea32bd3a6a18918b0a22fa942f
Gbenga <tolsoft32@gmail.com>
2023-07-10 15:30:06 -0700

    create file function

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * create_file - function that create file
+ * @filename: file name to be created
+ * @text_content: pointer address
+ *
+ * Return: -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int o_file, w_file, i = 0;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		for  (i = 0; text_content[i];)
+			i++;
+	}
+	o_file = open(filename, O_CREATE | O_RDWR | O_TRUNC, 0600);
+	w_file = write(o_file, text_content, i);
+
+	if (o_file == -1 || w_file == -1)
+		return (-1);
+
+	close(o_file);
+	return (1);
+}
