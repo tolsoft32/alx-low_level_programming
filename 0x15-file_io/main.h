39ed3cc31de110785e300cb5787cfdda333dc99e
Gbenga <tolsoft32@gmail.com>
2023-07-10 15:16:02 -0700

    Main heading ajustment

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int _putchar(char c);
