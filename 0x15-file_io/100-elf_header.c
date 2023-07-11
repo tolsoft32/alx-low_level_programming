21fb23f19f449dacf8752139df651a9ad3da54cf
Gbenga <tolsoft32@gmail.com>
2023-07-10 17:08:57 -0700

    elf descriptor

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point to program elf file
+ * @argc: number of arguments
+ * @argv: pointer to array of argument
+ *
+ * Return: 1 on success, error on failure
+ */
+int main(int argc, char *argv[])
+{
+	printf("Number of argument: %d\n", argc);
+
+	for (int i = 0; i < argc; i++)
+		printf("Argument %d\n: %s\n", i, argv[i]);
+	return (1);
+}
