53027c59d8f36b752df9462060aa8e005c217ad0
Gbenga <tolsoft32@gmail.com>
2023-08-03 08:00:16 -0700

    Binary to Unit upd

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -12,17 +13,17 @@ unsigned int binary_to_uint(const char *b)
 	int i;
 	unsigned int b_ten;
 
-	if (!b)
-	{
+	if (b == NULL)
 		return (0);
-	}
-	for (i = 0; b[i]; i++)
+
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (b[i] < '0' || b[i] > '1')
-		{
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		}
-		b_ten = 2 * b_ten + (b[i] - '0');
+
+		b_ten = b_ten << 1;
+		if (b[i] == '1')
+			b_ten = b_ten | 1;
 	}
 	return (b_ten);
 }
