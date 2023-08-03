b9968b70baa17b5131a3334cb795e0fc6744bd03
Gbenga <tolsoft32@gmail.com>
2023-08-03 08:41:48 -0700

    set bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,10 +9,14 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
+	unsigned long int pk;
+
 	if (index >= (sizeof(unsigned long int) * 8))
+	{
 		return (-1);
+	}
 
-	*n |= 1UL << index;
-
-	return (-1);
+	pk = 1 << index;
+	*n  = (*n | pk);
+	return (1);
 }
