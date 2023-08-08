8f9ac497a260e7ac10250b51c3bcb89bc7301024
Gbenga <tolsoft32@gmail.com>
2023-08-07 17:04:09 -0700

    Append file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,36 +8,22 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int file_d;
+	int file_d, i, read_w;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
+	file_d = open(filename, O_WRONLY | O_APPEND);
 
-	file_d = open(filename, O_WRONLY, O_APPEND);
 	if (file_d == -1)
 		return (-1);
-
 	if (text_content)
 	{
-		if (write(file_d, _strlens(text_content)) == -1)
+		for (i = 0; text_content[i]; i++)
+			;
+		read_w = write(file_d, text_content, i);
+		if (read_w == -1)
 			return (-1);
 	}
-
 	close(file_d);
 	return (1);
-}
-
-/**
- * _strlens - function to return length of strings
- * @s: counting strings
- * Return: string length
- */
-int _strlens(char *s)
-{
-	int count;
-
-	while (s[count])
-		count++;
-
-	return (count);
 }
